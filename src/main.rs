074ee0c79fb61d67849877a6d221112e6638a382
Aniruddha <azizul.muktadir.chowdhury@g.bracu.ac.bd>
2024-09-01 18:24:23 +0600

    first integration test green

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,8 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-/* async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-} */
-
-async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
-}
-
-
+//! main.rs
+use std::net::TcpListener;
+use zero2prod::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    let address = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
+    run(address)?.await
 }
